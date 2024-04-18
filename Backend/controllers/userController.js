0ee7db78de4be09e6c70f9bf8837258875dc8ab8
Saurabh Chaudhary <saurabh@Saurabh>
2024-04-18 15:06:57 +0530

    small updation

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,7 @@
-const User = require("./../models/userModel");
+const User = require("./../models/userModal");
 const catchAsync = require("./../utils/catchAsync");
 
-exports.getAllUsers = catchAync(async (req, res,next) => {
+exports.getAllUsers = catchAsync(async (req, res,next) => {
   const users = await User.find();
 
   res.status(200).json({
