892d5da6b1e70fa245f024fb26100efe94197e26
Saurabh Chaudhary <Saurbhbailara@gmail.com>
2024-05-08 15:22:10 +0530

    port updated

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ mongoose
  .connect(process.env.DATABASE)
   .then(() => console.log("DB connection successfully!"));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
